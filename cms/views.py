5e737329637111c6c5edf4b7db71c5546a586866
Joswaldo <joswaldogomezlara@gmail.com>
2016-04-05 20:43:34 +0200

    Ejercicio 15.5

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import Pages
 
 # Create your views here.
+
+def new_page(request, page_name):
+
+    page = Pages(name=page_name, page='You are in the page ' + page_name)  
+    page.save()
+
+    return HttpResponse(page.page)
